1c658cdef007280f448292118cc8281e9a77bcd2
htetmyatthar <htetmyathm2002@gmail.com>
2023-12-18 15:55:19 +0630

    added more comments

--- ps0-template/count_long_subarray.go
+++ ps0-template/count_long_subarray.go
@@ -1,7 +1,12 @@
-package count_long_subarray
+// package count_long_subarray provides the function for counting the logest
+// subarray in a positive number array.
+package count_long_subarray // import "https://github.com/htetmyatthar/6006/ps0-template"
 
-// Input:  A     | Golang array of positive integers
-// Output: count | number of longest increasing subarrays of A
+// CountLongSubarray takes a Golang array of positive integers and returns the
+// number of longest increasing subarrays.
+//
+// Input:  A     | Golang array of positive integers.
+// Output: count | number of longest increasing subarrays of A.
 func CountLongSubarray(A []int) int {
 	currentCount := 0
 	currentLength := 0
@@ -12,13 +17,13 @@ func CountLongSubarray(A []int) int {
 		// if this is not the last element and such
 		if (i+1) != len(A) && A[i] < A[i+1] {
 			iterateLength += 1
-		// either it was list index out of range or the first element is bigger than the second element
+			// either it was list index out of range or the first element is bigger than the second element
 		} else {
 			// if current checking longest length is bigger than the existing longest length
 			if iterateLength > currentLength {
 				currentCount = 1
 				currentLength = iterateLength
-			// elif current checking longest length is equal to the existing logest length
+				// elif current checking longest length is equal to the existing logest length
 			} else if iterateLength == currentLength {
 				currentCount++
 			}
