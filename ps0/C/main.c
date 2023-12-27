6967eb0f4dd2412c5b1d4d9baa4897d3a5ec6bb4
htetmyatthar <htetmyathm2002@gmail.com>
2023-12-28 03:01:52 +0630

    trying some c and file organization

---
+++ ps0/C/main.c
@@ -0,0 +1,6 @@
+#include "utils.h"
+
+int main(void){
+	hello();
+	return 0;
+}
---
+++ ps0/C/utils.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "utils.h"
+
+int hello(void){
+	printf("hello, htetmyatthar\n");
+	return 0;
+}
