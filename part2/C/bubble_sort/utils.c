348928e6d8df9fbffcdbaa3a53001484c5386720
htetmyatthar <htetmyathm2002@gmail.com>
2024-01-01 21:59:53 +0630

    fix the sort

--- part2/C/bubble_sort/utils.c
+++ part2/C/bubble_sort/utils.c
@@ -10,7 +10,7 @@ void bubble_sort(int array[], int length)
 	*/
 	for(int i = 0; i < length; i++)
 	{
-		for (int j = 0; j < length - 1; j++)
+		for (int j = 0; j < length - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
