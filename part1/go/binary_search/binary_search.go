8b28ec568b107f1655f76173c4d66463d1cf2377
htetmyatthar <htetmyathm2002@gmail.com>
2024-01-07 23:39:45 +0630

    formatted

--- part1/go/binary_search/binary_search.go
+++ part1/go/binary_search/binary_search.go
@@ -4,22 +4,22 @@ import "errors"
 
 // Search the value in values slice, and returns a boolean.
 // if the given values slice is empty Can't operate on empty slice error is returned.
-func BinarySearch(values []int, value int)(bool, error){
-	if len(values) == 0{
+func BinarySearch(values []int, value int) (bool, error) {
+	if len(values) == 0 {
 		return false, errors.New("Can't operate on empty slice.")
 	}
-	left := 0;
-	right := len(values) - 1;
+	left := 0
+	right := len(values) - 1
 
-	for(left <= right) {
+	for left <= right {
 		// for preventing integer overflow, although go optimized for not overflowing.
 		middle := left + ((right - left) / 2)
-		if values[middle] == value{
+		if values[middle] == value {
 			return true, nil
 		} else if values[middle] < value {
-			left = middle + 1;
+			left = middle + 1
 		} else if values[middle] > value {
-			right = middle - 1;
+			right = middle - 1
 		}
 	}
 	return false, nil
