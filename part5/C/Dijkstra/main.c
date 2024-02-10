4998ceb44ce7e21e65ef166ed972294efeeb998d
htetmyatthar <htetmyathm2002@gmail.com>
2024-02-10 17:29:51 +0630

    added more edges e.g.

--- part5/C/Dijkstra/main.c
+++ part5/C/Dijkstra/main.c
@@ -2,6 +2,8 @@
 
 void adding_edges(graph* graph_adj);
 
+void adding_edges_more(graph *graph_adj);
+
 void print_parents(char parents[]);
 
 void print_shortest_paths(shortest_distances *distances);
@@ -9,6 +11,7 @@ void print_shortest_paths(shortest_distances *distances);
 int main(void)
 {
 	printf("Creating a graph.\n");
+	//char vertices[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'};
 	char vertices[] = {'a', 'b', 'c', 'd', 's'};
 	int length = sizeof(vertices) / sizeof(vertices[0]);
 	graph* Graph = create_graph(vertices, length);
@@ -16,12 +19,14 @@ int main(void)
 	print_hr();
 
 	printf("Adding edges to form a graph with no negative weight edges.\n");
+	//adding_edges_more(Graph);
 	adding_edges(Graph);
 	print_adjancency_list(Graph);
 	print_hr();
 
 	printf("Shortest paths\n");
 	char *parent_pointers = calloc(MAX_CHAR, sizeof(char));
+	//shortest_distances *distances = Dijkstra(Graph, 'S', parent_pointers);
 	shortest_distances *distances = Dijkstra(Graph, 's', parent_pointers);
 	if(distances != NULL)
 	{
@@ -49,6 +54,30 @@ void adding_edges(graph* graph_adj)
 	add_edge(graph_adj, 'c', 'd', 2);
 	add_edge(graph_adj, 'b', 'd', 7);
 	add_edge(graph_adj, 'd', 'b', 5);
+}
+
+void adding_edges_more(graph *graph_adj)
+{
+	add_edges(graph_adj, 'A', (char[]){'B', 'C', 'D', 'E'}, (int[]){1, 2, 3, 4}, 4);
+	add_edges(graph_adj, 'B', (char[]){'A', 'C', 'D', 'E', 'F'}, (int[]){1, 2, 3, 4, 5}, 5);
+	add_edges(graph_adj, 'C', (char[]){'A', 'B', 'D', 'E', 'F'}, (int[]){2, 2, 3, 4, 5}, 5);
+	add_edges(graph_adj, 'D', (char[]){'A', 'B', 'C', 'E', 'F'}, (int[]){3, 3, 3, 4, 5}, 5);
+	add_edges(graph_adj, 'E', (char[]){'A', 'B', 'C', 'D', 'F', 'G'}, (int[]){4, 4, 4, 4, 5, 6}, 6);
+	add_edges(graph_adj, 'F', (char[]){'B', 'C', 'D', 'E', 'G', 'H'}, (int[]){5, 5, 5, 5, 6, 7}, 6);
+	add_edges(graph_adj, 'G', (char[]){'E', 'F', 'H', 'I'}, (int[]){6, 6, 7, 8}, 4);
+	add_edges(graph_adj, 'H', (char[]){'F', 'G', 'I', 'J'}, (int[]){7, 7, 8, 9}, 4);
+	add_edges(graph_adj, 'I', (char[]){'G', 'H', 'J', 'K'}, (int[]){8, 8, 9, 10}, 4);
+	add_edges(graph_adj, 'J', (char[]){'H', 'I', 'K', 'L'}, (int[]){9, 9, 10, 11}, 4);
+	add_edges(graph_adj, 'K', (char[]){'I', 'J', 'L', 'M'}, (int[]){10, 10, 11, 12}, 4);
+	add_edges(graph_adj, 'L', (char[]){'J', 'K', 'M', 'N'}, (int[]){11, 11, 12, 13}, 4);
+	add_edges(graph_adj, 'M', (char[]){'K', 'L', 'N', 'O'}, (int[]){12, 12, 13, 14}, 4);
+	add_edges(graph_adj, 'N', (char[]){'L', 'M', 'O', 'P'}, (int[]){13, 13, 14, 15}, 4);
+	add_edges(graph_adj, 'O', (char[]){'M', 'N', 'P', 'Q'}, (int[]){14, 14, 15, 16}, 4);
+	add_edges(graph_adj, 'P', (char[]){'N', 'O', 'Q', 'R'}, (int[]){15, 15, 16, 17}, 4);
+	add_edges(graph_adj, 'Q', (char[]){'O', 'P', 'R', 'S'}, (int[]){16, 16, 17, 18}, 4);
+	add_edges(graph_adj, 'R', (char[]){'P', 'Q', 'S', 'T'}, (int[]){17, 17, 18, 19}, 4);
+	add_edges(graph_adj, 'S', (char[]){'Q', 'R', 'T'}, (int[]){18, 18, 19}, 3);
+	add_edges(graph_adj, 'T', (char[]){'R', 'S'}, (int[]){19, 19}, 2);
 }
 
 // print_shortest_paths prints out the values of the shortest paths to the vertices.
