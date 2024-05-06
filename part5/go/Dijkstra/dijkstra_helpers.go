f8dc921d36eb67b91dc465099b8d2d42ec779434
htetmyatthar <htetmyathm2002@gmail.com>
2024-05-06 14:48:07 +0630

    FIX: fixed the wrong implementation of changeable priority queue

--- part5/go/Dijkstra/dijkstra_helpers.go
+++ part5/go/Dijkstra/dijkstra_helpers.go
@@ -9,97 +9,116 @@ func (G *Graph) Dijkstra(src rune) (*ShortestDistance, error) {
 	if G == nil {
 		return nil, ErrGraphNil
 	}
-	queue := make(ChangablePriorityQueue, G.totalVertices, G.totalVertices)
+
+	pq := make(PriorityQueue, G.totalVertices, G.totalVertices)
 	sd := &ShortestDistance{Src: src, Distances: make(map[rune]*DistanceNode, G.totalVertices)}
 	i := 0
 	for vertexName := range G.adjList {
 		// initialize the structs and distances to infinity.
 		sd.Distances[vertexName] = &DistanceNode{Distance: math.Inf(0)}
 		// storing data for the changable priority queue.
-		queue[i] = &QueueItem{
+		pq[i] = &PQItem{
 			value:    vertexName,
 			priority: sd.Distances[vertexName].Distance,
 			index:    i,
 		}
 		i++
 	}
-	heap.Init(&queue) // init the changable priority queue.
+	// initialize the changeable priority queue.
+	cpq := &ChangeablePriorityQueue{}
+	cpq.Init(pq)
 
 	// initialize the source.
 	sd.Distances[src].Distance = 0
 	sd.Distances[src].ParentVertex = src
+	cpq.DecreaseKey(src, 0) // update the priority of the src
 
 	// while Q is not empty, delete item(u, d(s, u)) from Q that is minimum.
-	for queue.Len() != 0 {
-		item := queue.Pop()
-		value := item.(*QueueItem).value.(rune)
-		// for every edges.
+	for cpq.Len() > 0 {
+		item := cpq.DeleteMin().(*PQItem)
+		value := item.value.(rune)
 		for e := G.adjList[value].edges.Front(); e != nil; e = e.Next() {
 			edge := e.Value.(*Edge)
 			G.tryToRelax(sd, value, edge)
-			queue.Update(item.(*QueueItem), value, float64(edge.weight))
+			cpq.DecreaseKey(edge.vertixName, float64(edge.weight))
+			// queue.Update(item.(*QueueItem), edge.vertixName, float64(edge.weight))
 		}
 	}
 	return sd, nil
 }
 
-type QueueItem struct {
-	value    any     // the value of the item; arbitrary.
-	priority float64 // the priority of the item in the queue.
-	// The index is needed by update and is maintained by the heap.Interface method.
-	index int // the index of the item in the heap.
+type element any
+
+type ChangeablePriorityQueue struct {
+	pqMap map[element]*PQItem // maps the vertex rune to Queue item.
+	pq    PriorityQueue
 }
 
-// A ChangablePriorityQueue implements heap.Interface and holds the QueueItems.
-// This is to use with the Dijkstra algorithm.
-//
-// to intialize the ChangablePriorityQueue:
-//
-// pq := make(PriorityQueue, len(items))	// make the queue items and add them if neccessary.
-//
-// heap.Init(&pq)	// initialize the heap interface.
-type ChangablePriorityQueue []*QueueItem
-
-func (cpq ChangablePriorityQueue) Len() int { return len(cpq) }
-
-func (cpq ChangablePriorityQueue) Less(i, j int) bool {
-	// we want the lowest, use less than
-	return cpq[i].priority < cpq[j].priority
+// Init initialize the given pq and sets it to the Pq field of the ChangeablePriorityQueue.
+func (cpq *ChangeablePriorityQueue) Init(pq PriorityQueue) {
+	cpq.pqMap = make(map[element]*PQItem, pq.Len())
+	// maps the vertex rune to the priority queue.
+	for _, item := range pq {
+		cpq.pqMap[item.value.(element)] = item
+	}
+	heap.Init(&cpq.pq)
+	cpq.pq = pq
 }
 
-func (cpq ChangablePriorityQueue) Swap(i, j int) {
-	cpq[i], cpq[j] = cpq[j], cpq[i]
-	cpq[i].index = i
-	cpq[j].index = j
+// Len returns the number of items in the queue.
+func (cpq *ChangeablePriorityQueue) Len() int {
+	return cpq.pq.Len()
 }
 
-func (cpq *ChangablePriorityQueue) Push(x any) {
-	n := len(*cpq)
-	item := x.(*QueueItem)
+// DecreaseKey update the key(priority) of this queue to the given priority.
+func (cpq *ChangeablePriorityQueue) DecreaseKey(vertexName rune, priority float64) {
+	item := cpq.pqMap[vertexName]
+	item.priority = priority
+	heap.Fix(&cpq.pq, item.index)
+}
+
+// DeleteMin removes the least priority item and returns it.
+func (cpq *ChangeablePriorityQueue) DeleteMin() any {
+	return heap.Pop(&cpq.pq)
+}
+
+// An PQItem is something we manage in a priority queue.
+type PQItem struct {
+	value    any     // The value of the item; arbitrary.
+	priority float64 // The priority of the item in the queue.
+	// The index is needed by update and is maintained by the heap.Interface methods.
+	index int // The index of the item in the heap.
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*PQItem
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	return pq[i].priority < pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x any) {
+	n := len(*pq)
+	item := x.(*PQItem)
 	item.index = n
-	*cpq = append(*cpq, item)
+	*pq = append(*pq, item)
 }
 
-// Pop removes the least priority item and returns it.
-//
-// this implementation can be used as delete_min operation for Dijkstra algorithm.
-func (cpq *ChangablePriorityQueue) Pop() any {
-	old := *cpq
+func (pq *PriorityQueue) Pop() any {
+	old := *pq
 	n := len(old)
 	item := old[n-1]
 	old[n-1] = nil  // avoid memory leak
 	item.index = -1 // for safety
-	*cpq = old[0 : n-1]
+	*pq = old[0 : n-1]
 	return item
-}
-
-// Update modifies the priority and value of an QueueItem in the priority queue.
-// Value's type must be the same as the type that is stored inside the value field of the QueueItem.
-//
-// this implementation can be used as decrease_key operation for Dijkstra algorithm.
-// priority is the shortest distance from src vertex to the value vertex.
-func (cpq *ChangablePriorityQueue) Update(item *QueueItem, value any, priority float64) {
-	item.value = value
-	item.priority = priority
-	heap.Fix(cpq, item.index)
 }
